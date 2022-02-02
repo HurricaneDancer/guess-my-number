6ddac575f360c7075c56ee9203b6d21fb9000a08
HurricaneDancer <hurricane@hurricanedancer.com>
2022-02-02 14:55:36 +0200

    done

---
+++ README.md
@@ -0,0 +1,16 @@
+<h1>Guess My Number</h1>
+
+<p>
+Guess my number is a game which is part of <a href="https://www.udemy.com/user/jonasschmedtmann/">Jonas Schmedtmann</a>'s JavaScript course on Udemy. This is my implementation of the game (I did this before looking at the solution or following his tutorial)
+
+In this game, you must guess the secret number that has been generated (Don't cheat with the console 😡)</br>
+If you guess too low or too high, the game will tell you.</br>
+If you win, the screen will change to green and the message will update.</br>
+If you lose, the screen will change to red and the message will update.</br>
+You can reset the game by clicking "Again!" and it will update your scores and the screen.</br>
+
+The point of this lesson was DOM manipulation </br>
+
+PS. The font is a google font 🤷‍♂️
+
+</P>
---
+++ script.js
@@ -0,0 +1,74 @@
+'use strict';
+const generateNumber = () => Math.trunc(Math.random() * 20) + 1;
+
+let currentScore = 20;
+let highScore = 0;
+let secretNumber = generateNumber();
+
+// update the test of the element with the .message class
+const updateMessage = message =>
+  (document.querySelector('.message').textContent = message);
+
+// update the background color of the body
+const updateBackground = color =>
+  (document.querySelector('body').style.backgroundColor = color);
+
+// update the element with the score
+const updateScore = () =>
+  (document.querySelector(
+    '.label-score'
+  ).textContent = `💯 Score ${currentScore}`);
+
+// reset the game by resetting the secret number, the score to 20, the background color and the message
+const resetGame = function () {
+  secretNumber = generateNumber();
+  currentScore = 20;
+  updateScore();
+  updateBackground('#222');
+  updateMessage('Start guessing...');
+};
+
+// if the guess is correct then run the win condition, otherwise reduce the score
+const guessChecker = function (guess) {
+  if (guess === secretNumber) return winCondition();
+
+  currentScore--;
+  updateScore();
+  updateMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+};
+
+// if their highscore is greater than current, update it and the screen
+const winCondition = function () {
+  if (currentScore > highScore) {
+    highScore = currentScore;
+    document.querySelector(
+      '.label-highscore'
+    ).textContent = `🥇 Highscore: ${highScore}`;
+  }
+  updateMessage('You win!');
+  updateBackground('green');
+};
+
+// set their score to 0, and update the screen
+const loseCondition = function () {
+  currentScore = 0;
+  updateScore();
+  updateBackground('red');
+  updateMessage('You lose! Play again?');
+};
+
+// validate if their guess is still > 1, otherwise run a lose condition
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = +document.querySelector('.guess').value;
+  if (!guess) return updateMessage('Please guess a number');
+
+  if (currentScore > 1) {
+    guessChecker(guess);
+  } else {
+    loseCondition();
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  resetGame();
+});
